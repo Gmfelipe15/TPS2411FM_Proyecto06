ac88a330b109897dc94da124410dd17cc4350c8b
Estebankitoo <estebanquin500@gmail.com>
2024-09-05 06:40:53 -0500

    dfrd

--- PROYECTO_BEI/bei.py
+++ PROYECTO_BEI/bei.py
@@ -3,15 +3,25 @@ from flask_mysqldb import MySQL
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
+
+from dotenv import load_dotenv
+load_dotenv()  # Carga las variables del archivo .env esto para subir a RENDER
+
 import os
  
 app = Flask(__name__)
 
 #Conexión a mysql
-app.config['user'] = 'localhost'
+"""app.config['user'] = 'localhost'
 app.config['MYSQL_USER'] = 'syntax'
 app.config['MYSQL_PASSWORD'] = 'syntaxis'
 app.config['MYSQL_DB'] = 'bei'
+mysql = MySQL(app)"""
+#Conexion editada para subir a RENDER
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST','localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER','syntax')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD','syntaxis')
+app.config['MYSQL_DB'] =os.getenv('MYSQL_DB','bei')
 mysql = MySQL(app)
 
 # configuraciones para la conexión
