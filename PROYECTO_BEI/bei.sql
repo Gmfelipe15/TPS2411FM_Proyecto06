e60a4c7116ab747ab0ea50400770a4e33aa34299
Estebankitoo <juandavidbeltran2017@gmail.com>
2024-09-12 07:38:58 -0500

    hg

--- PROYECTO_BEI/bei.sql
+++ PROYECTO_BEI/bei.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-07-2024 a las 18:54:32
+-- Tiempo de generación: 29-08-2024 a las 18:14:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -34,6 +34,19 @@ CREATE TABLE `contactus` (
   `message` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `contactus`
+--
+
+INSERT INTO `contactus` (`id`, `name`, `email`, `message`) VALUES
+(1, 'primer registro', 'primer@registro.com', 'primerregistrO'),
+(2, 'AAAAAAAAAAAAA', 'AAAA@AAA.AA', 'AAAXAAAUn mago nunca llega tarde, Frodo Bolsón, ni pronto. · \"El camino va siempre, siempre hacia adelante\"'),
+(3, 'EEE', 'EEE@EEE.EE', 'EEEE'),
+(4, 'TEEE', 'AAAA@AAA.AA', 'aaaasasas'),
+(5, 'EEE', 'EEE@EEE.EE', 'EEEEEEE'),
+(6, 'Esteban Quintero Rodríguez', 'admin@admin.admin', 'Hola me gustaría saber cosas gracias!!!!!!! Soy Aragorn hijo de Arathorn y me llaman Elessar, Piedra de Elfo, Dúnadan, heredero del hijo de Isildur, hijo de Elendil de Gondor.'),
+(7, 'Felipincio Garcimba', 'felipeencio@gmla.com.co', 'No te apresures a dar muerte y juicio. Incluso los más sabios no ven todos los fines». (Gandalf). La sabiduría de Gandalf ');
+
 -- --------------------------------------------------------
 
 --
@@ -63,7 +76,7 @@ CREATE TABLE `signup` (
   `password` varchar(150) NOT NULL,
   `direccion` varchar(200) NOT NULL,
   `telefono` varchar(50) NOT NULL,
-  `tipo` varchar(255) NOT NULL
+  `tipo` tinyint(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
@@ -71,14 +84,18 @@ CREATE TABLE `signup` (
 --
 
 INSERT INTO `signup` (`id`, `name`, `email`, `password`, `direccion`, `telefono`, `tipo`) VALUES
-(3, 'oliwis', 'a3di74rrr@gm.com', 'asasas', 'SELOGROEDITAR', 'assass', 'ADMIN'),
-(5, 'NUEVO USER5', 'esteban.quintero699@edcionbogota.edu.co', '3eroooo', 'cra 101 assda', '423343', 'CLIENTE'),
-(7, 'XAAAAAA', 'aeaeae@gm.com', 'AAAAAAAAAAA', 'AAA', '444', 'CLIENTE'),
-(8, 'JUANDAPEDRAZALEON', 'xxesteban@gm.com', 'AAAAAAAA', 'KRA 101 c', '12121212', 'CLIENTE'),
-(9, 'ESTEBITANN', 'estebaaaquin500@mail.com', 'AAAAA', 'XXESTEBANXX', '12345678909876', 'CLIENTE'),
-(10, 'PIPEPUNK', 'luifelipelecra@gg.com', 'DANIELMUÑOZ', 'socratescalle1', '989898989', 'CLIENTE'),
-(11, 'GABRIELEPINILLA', 'xxestean@gm.com', 'GABRIELLE', '12121', '2222222', 'ADMIN'),
-(12, 'ADMINISTRADOR', 'admin@admin.admin', 'admin', 'admin123muñoz', '4dm1n', 'ADMIN');
+(3, 'SENA', 'sena@soysena.com', 'asasas', 'SENA', '53744568', 1),
+(5, 'NUEVO USER5', 'esteban.quintero699@edcionbogota.edu.co', '3eroooo', 'cra 101 assda', '423343', 0),
+(7, 'XAAAAAA', 'aeaeae@gm.com', 'AAAAAAAAAAA', 'AAA', '444', 0),
+(8, 'JUANDAPEDRAZALEON', 'xxesteban@gm.com', 'AAAAAAAA', 'KRA 101 c', '12121212', 0),
+(9, 'ESTEBITANN', 'estebaaaquin500@mail.com', 'AAAAA', 'XXESTEBANXX', '12345678909876', 0),
+(10, 'PIPEPUNK', 'luifelipelecra@gg.com', 'DANIELMUÑOZ', 'socratescalle1', '989898989', 0),
+(11, 'GABRIELEPINILLA', 'xxestean@gm.com', 'GABRIELLE', '12121', '2222222', 0),
+(12, 'ADMINISTRADOR', 'admin@admin.admin', 'admin', 'admin123muñoz', '4dm1n', 0),
+(13, ' registro', 'primer@registro.com', 'aa', 'aaa', 'aaa', 0),
+(14, 'AAAAAAAAAAAAA', 'AAAA@AAA.AA', 'AAAAAAA', 'AAAAAAA', 'AAAAAAA', 0),
+(15, 'k', 'k@k.k', 'k', 'k', 'k', 0),
+(16, 'X', 'x@x.x', 'x', 'x', 'x', 0);
 
 --
 -- Índices para tablas volcadas
@@ -110,7 +127,7 @@ ALTER TABLE `signup`
 -- AUTO_INCREMENT de la tabla `contactus`
 --
 ALTER TABLE `contactus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -122,7 +139,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `signup`
 --
 ALTER TABLE `signup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
