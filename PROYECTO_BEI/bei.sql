33971af8de8877ba2b9b9bc0dadc8cc35c32fdc0
Estebankitoo <juandavidbeltran2017@gmail.com>
2024-09-12 09:37:42 -0500

    ljhgkg

--- PROYECTO_BEI/bei.sql
+++ PROYECTO_BEI/bei.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-08-2024 a las 18:14:02
+-- Tiempo de generación: 12-09-2024 a las 16:36:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -80,6 +80,24 @@ CREATE TABLE `signup` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
+-- Volcado de datos para la tabla `signup`
+--
+
+INSERT INTO `signup` (`id`, `name`, `email`, `password`, `direccion`, `telefono`, `tipo`) VALUES
+(3, 'SENA', 'sena@soysena.com', 'asasas', 'SENA', '53744568', 1),
+(5, 'NUEVO USER5', 'esteban.quintero699@edcionbogota.edu.co', '3eroooo', 'cra 101 assda', '423343', 0),
+(7, 'XAAAAAA', 'aeaeae@gm.com', 'AAAAAAAAAAA', 'AAA', '444', 0),
+(8, 'JUANDAPEDRAZALEON', 'xxesteban@gm.com', 'AAAAAAAA', 'KRA 101 c', '12121212', 0),
+(9, 'ESTEBITANN', 'estebaaaquin500@mail.com', 'AAAAA', 'XXESTEBANXX', '12345678909876', 0),
+(10, 'PIPEPUNK', 'luifelipelecra@gg.com', 'DANIELMUÑOZ', 'socratescalle1', '989898989', 0),
+(11, 'GABRIELEPINILLA', 'xxestean@gm.com', 'GABRIELLE', '12121', '2222222', 0),
+(12, 'ADMINISTRADOR', 'admin@admin.admin', 'admin', 'admin123muñoz', '4dm1n', 0),
+(13, ' registro', 'primer@registro.com', 'aa', 'aaa', 'aaa', 0),
+(14, 'AAAAAAAAAAAAA', 'AAAA@AAA.AA', 'AAAAAAA', 'AAAAAAA', 'AAAAAAA', 0),
+(15, 'k', 'k@k.k', 'k', 'k', 'k', 0),
+(16, 'X', 'x@x.x', 'x', 'x', 'x', 0);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -127,4 +145,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
