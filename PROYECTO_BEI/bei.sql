042356735dd2d6e4ae7782405c8cf14641b41294
Estebankitoo <estebanquin500@gmail.com>
2024-10-03 05:09:47 -0500

    Actualizando base de datos

--- PROYECTO_BEI/bei.sql
+++ PROYECTO_BEI/bei.sql
@@ -1,7 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
 CREATE TABLE `contactus` (
   `id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL,
@@ -9,15 +5,14 @@ CREATE TABLE `contactus` (
   `message` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
-
 CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(80) NOT NULL,
   `descripcion` varchar(255) DEFAULT 'Sin descripción',
-  `precio` int(10) NOT NULL,
+  `precio` varchar(10) NOT NULL,
   `cantidad` int(5) DEFAULT 1,
-  `imagen` varchar(30) NOT NULL,
-  `disponibilidad` varchar(15) DEFAULT 'Disponible'
+  `imagen` varchar(30) DEFAULT 'Sin imagen',
+  `disponibilidad` varchar(30) DEFAULT 'Disponible'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 CREATE TABLE `signup` (
@@ -39,13 +34,13 @@ ALTER TABLE `productos`
 ALTER TABLE `signup`
   ADD PRIMARY KEY (`id`);
 
+--
 ALTER TABLE `contactus`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 ALTER TABLE `signup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
-
