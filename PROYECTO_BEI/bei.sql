0542dcfc7cc0f9a18cffcbea40cc5c18747e9098
Estebankitoo <estebanquin500@gmail.com>
2024-10-03 06:42:20 -0500

    basedatatata

--- PROYECTO_BEI/bei.sql
+++ PROYECTO_BEI/bei.sql
@@ -1,9 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2024 a las 13:40:57
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bei`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactus`
+--
+
 CREATE TABLE `contactus` (
   `id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL,
   `email` varchar(60) NOT NULL,
   `message` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
 
 CREATE TABLE `productos` (
   `id` int(11) NOT NULL,
@@ -15,6 +50,17 @@ CREATE TABLE `productos` (
   `disponibilidad` varchar(30) DEFAULT 'Disponible'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `signup`
+--
+
 CREATE TABLE `signup` (
   `id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL,
@@ -25,22 +71,54 @@ CREATE TABLE `signup` (
   `tipo` tinyint(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `signup`
+--
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `contactus`
+--
 ALTER TABLE `contactus`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `productos`
+--
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `signup`
+--
 ALTER TABLE `signup`
   ADD PRIMARY KEY (`id`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contactus`
+--
 ALTER TABLE `contactus`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
 ALTER TABLE `productos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
+--
+-- AUTO_INCREMENT de la tabla `signup`
+--
 ALTER TABLE `signup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
